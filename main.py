5926b6299c57756d3e29a4a30b671c02110e451e
Pushpendra514 <pushpendra2242005@gmail.com>
2026-03-12 18:58:01 +0530

    add open post feature

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ data_json = data.json()
 app = Flask(__name__)
 @app.route("/")
 def home_fun():
-    return render_template("index.html",file_location="assets/img/home-bg.jpg",blogs_data=data_json)
+    return render_template("index.html",file_location="static/assets/img/home-bg.jpg",blogs_data=data_json)
 @app.route("/about")
 def about_fun():
-    return render_template("about.html",file_location="assets/img/about-bg.jpg")
+    return render_template("about.html",file_location="static/assets/img/about-bg.jpg")
 @app.route("/contact")
 def contact_fun():
-    return render_template("contact.html",file_location="assets/img/contact-bg.jpg")
+    return render_template("contact.html",file_location="static/assets/img/contact-bg.jpg")
 @app.route("/post/<int:post_id>")
 def post_fun(post_id):
 
@@ -20,7 +20,9 @@ def post_fun(post_id):
         if content["id"] == post_id:
             #Problem there fix when we click any post redirect
             # to individual blog post page using url_for
-            file_des = content["image_url"]
-            return render_template("post.html",file_location=file_des)
+            # file_des = content["image_url"]
+            # title = content["title"]
+            return render_template("post.html",post_content=content)
+    return "error"
 if __name__ == "__main__":
     app.run(debug=True)
